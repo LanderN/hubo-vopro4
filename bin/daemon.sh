8cdbee2f71645fbb433b0cbc386444539ceffd46
Lander Noterman <lander.noterman@ugent.be>
2016-03-24 17:22:47 +0100

    remove hubtrc from daemon

--- bin/daemon.sh
+++ bin/daemon.sh
@@ -8,13 +8,11 @@ PIDFILE=$HUBOT_ROOT/hubot.pid
 case "$1" in
 start)
         echo "Starting"
-        . $HUBOT_ROOT/bin/.hubotrc
         /sbin/start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile -d $HUBOT_ROOT --exec $DAEMON
         echo "."
         ;;
 debug)
         echo "Debug mode: no backgrounding"
-        . $HUBOT_ROOT/bin/.hubotrc
         /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile -d $HUBOT_ROOT --exec $DAEMON
         echo "."
         ;;        
@@ -26,7 +24,6 @@ stop)
 restart)
         echo "Restarting"
         /sbin/start-stop-daemon --stop --pidfile $PIDFILE
-        . $HUBOT_ROOT/bin/.hubotrc
         /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile --background -d $HUBOT_ROOT --exec $DAEMON
         echo "."
         ;;
